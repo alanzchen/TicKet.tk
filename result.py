503680a7a935d98ec9f5500f267db0709f1faac5
Discrete Chan <c.discrete@gmail.com>
2013-01-21 20:09:12 +0800

    A brand new UI.

--- result.py
+++ result.py
@@ -1,7 +1,19 @@
 #-*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.template import Context, loader
-def result(request,origin,target,date):
+from django.utils import simplejson
+from dajaxice.decorators import dajaxice_register
+
+@dajaxice_register
+def result(request,origin,target,date)
+	dajax = Dajax()
+	dajax.assign('#origin', 'value', origin)
+	dajax.assign('#target', 'value', target)
+	dajax.assign('#date', 'value', date)
+	
+
+
+
 	indextemplate = loader.get_template('result.html')
 	ctt=Context()
 	ctt['h1']='Congratulation!'
