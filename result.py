6da6d2ae10d75e001f94c88c6e977c81ce707540
Discrete Chan <c.discrete@gmail.com>
2013-01-30 21:39:35 +0800

    Bugs fixed

--- result.py
+++ result.py
@@ -4,12 +4,12 @@ from django.template import Context, loader
 from django.utils import simplejson
 from ticket.models import PlaneDB
 
-def result(request,source,target)
+def result(request, source, target):
 	indextemplate = loader.get_template('result.html')
-	Plane = PlaneDB.object.filter(Source=source).filter(Target=target)
+	Plane = PlaneDB.objects.filter(Source=source).filter(Target=target)
 	ctt=Context()
 	ctt['h1']='Congratulation!'
 	ctt['title']='结果'
 	ctt['source']=source
 	ctt['target']=target
-	return dajax.json()
+	return HttpResponse(indextemplate.render(ctt))
--- settings.py
+++ settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/discrete/projects/ticket/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -64,6 +64,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    "/home/discrete/projects/static/",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -75,7 +76,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    'dajaxice.finders.DajaxiceFinder',
+ #   'dajaxice.finders.DajaxiceFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -107,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/discrete/workspace/ticket/ticket/templates/"
+    "/home/discrete/projects/ticket/templates/"
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
