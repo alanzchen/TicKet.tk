e7759926690215251370b536399b33b83745c1f0
Discrete Chan <c.discrete@gmail.com>
2013-02-03 00:05:40 +0800

    我居然忘记传了……一些改动。

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Data(models.Model):
+    Source = models.CharField(max_length=3)
+    Target = models.CharField(max_length=3)
+    Cost = models.IntegerField()
+    PlaneNum = models.IntegerField()
+    Plane1 = models.CharField(max_length=255)
+    Plane2 = models.CharField(max_length=255)
+    Plane3 = models.CharField(max_length=255)
+    Plane4 = models.CharField(max_length=255)
+    Plane5 = models.CharField(max_length=255)
+    Plane6 = models.CharField(max_length=255)
+    Plane7 = models.CharField(max_length=255)
+    Plane8 = models.CharField(max_length=255)
+    Plane9 = models.CharField(max_length=255)
+    Plane10 = models.CharField(max_length=255)
