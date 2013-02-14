8bbe3013e13344acb904dba4f961429ad73df12a
EnigmaHuang <enigmahuang@gmail.com>
2013-02-14 16:51:58 +0800

    update

--- backend-core/SPS.pas
+++ backend-core/SPS.pas
@@ -1,150 +1,117 @@
-﻿program SPS;
+unit SysData;
 
 {$mode objfpc}{$H+}
 
+interface
+
 uses
-  {$IFDEF UNIX}{$IFDEF UseCThreads}
-  cthreads,
-  {$ENDIF}{$ENDIF}
-  Classes, SysUtils, CustApp,
-  Citys, SysData, SearchKernel, DatabaseIO, SysDAT
-  { you can add units after this };
+  Classes, SysUtils, Citys, DB, sqlite3ds, SysDAT;
 
-type
+{$Define Release}
 
-  { TConsoleServer }
+const
 
-  TConsoleServer = class(TCustomApplication)
-  protected
-    procedure DoRun; override;
-  public
-    constructor Create(TheOwner: TComponent); override;
-    destructor Destroy; override;
-    procedure WriteHelp; virtual;
+  MaxDay = 2;
+  MaxJump = 10;
+  FillCharMax = 16843009;
+
+  {$IFDEF DebugMode}
+  RouterNumber = 5;
+  MaxTimeSection = 20+1;
+  {$ENDIF}
+
+  {$IFDEF Release}
+  RouterNumber = CityNumber;
+  MaxTimeSection = MaxDay*24*2+1;  //注意！为什么要加1呢？因为搜索的时候会
+  {$ENDIF}                         //有越出边界1个数组的临界状态，需要处理
+
+  _Day1 = 1;
+  _Day2 = 2;
+
+  ver        = 'LianYao.Q' ;       //任何版本长度请使用9字节定长
+                                   //上一版本是TEST TYPE    
+
+Type
+  PlaneInfo = record
+    SourceCode : string;    //出发地三字码
+    SourceID   : integer;   //出发地ID
+    TargetCode : string;    //目的地三字码
+    TargetID   : integer;   //目的地ID
+    PlaneName  : string;    //航班名称
+    Price      : integer;   //航班价格
+    LeaveTime  : integer;   //出发时间段
+    ArriveTime : integer;   //到达时间段
+    { 时间段的定义：每半个小时称为一个时间段，这样抽象处理数据
+      如果一个航班3：20出发，那么它的LeaveTime就是(3-1)×2+1=5
+      如果一个航班14：45出发，那么它的LeaveTime就是(14-1)×2+1+1=28
+      如果一个航班8：00出发，那么处理LeaveTime为(8-1)×2=14
+      类似地，对于到达时间ArriveTime，给出以下几个参考：
+      4：25到达 ArriveTime=(4-1)×2+1=7
+      6：55到达 ArriveTime=(6-1)×2+1+1=12
+      8：00到达 ArriveTime=(8-1)×2=14                               }
   end;
 
-{ TConsoleServer }
-
-{
-关于输入的格式:
-如果只有一天的数据，则为"ExeName 1day %第一天数据库路径% %写出数据库路径%"
-如果只有两天的数据，则为"ExeName 2day %第一天数据库路径% %第二天数据库路径% %写出数据库路径%"
-}
-procedure TConsoleServer.DoRun;
-var
-  Days,tgr:string;
-  id,day,i:integer;
-  cmdline:string;
-  EcptJump:boolean;
-begin
-  OpenLog;                       //打开日志文件
-  Days:=ParamStr(1);             //载入多少天的数据
-  day:=-1;
-  if Days='1day' then day:=1;
-  if Days='2day' then day:=2;
-  ResetData;
-
-  //取几天的数据库的文件路径
-  Day1DB:=ParamStr(2);
-  Day2DB:=ParamStr(3);
-
-  cmdline:='';
-  for i:=0 to 4 do cmdline:=cmdline+ParamStr(i)+' ';
-  AddLog('Start up command line is : '+cmdline);
-
-  Case day of
-    1:begin
-        Day1DBText:=Day1DB;
-        OutputDB:=Day2DB;                       //只有一天时，第三个参数为输出的数据库名字
-        TotalTime:=day*24*2;
-        LoadDBFromText(Day1DBText,1);           //读入数据
-        AddLog('Load database 1 over.');
-        for id:=1 to RouterNumber do
-        begin
-          EcptJump:=true;
-          tgr:=GetTWCodeByID(id);
-          AddLog('Now calculating target to '+tgr);
-          try
-            DPSearch(tgr);
-            AddLog('Calculation of target to '+tgr+' over. Now writing database.');
-            EcptJump:=false;
-          except
-            on E:Exception do AddLog('Exception happened. Details : '+E.ClassName+':'+E.Message);
-          end;
-          if EcptJump then continue;
-          try
-            WriteDataToSQLite(tgr);
-            AddLog('Write database over.');
-          except
-            on E:Exception do AddLog('Exception happened. Details : '+E.ClassName+':'+E.Message);
-          end;
-        end;
-        AddLog('Calculation over. Now terminate.');
-      end;
-    2:begin
-        Day1DBText:=Day1DB;
-        LoadDBFromText(Day1DBText,1);          //读入数据
-        AddLog('Load database 1 over.');
-        Day2DBText:=Day2DB;
-        LoadDBFromText(Day2DBText,1*2*24+1);   //读入数据
-        AddLog('Load database 2 over.');
-        OutputDB:=ParamStr(4);                 //只有两天时，第四个参数为输出的数据库名字
-        TotalTime:=day*24*2;
-        for id:=1 to RouterNumber do
-        begin
-          EcptJump:=false;
-          tgr:=GetTWCodeByID(id);
-          AddLog('Now calculating target to '+tgr);
-          try
-            DPSearch(tgr);
-            AddLog('Calculation of target to '+tgr+' over. Now writing database.');
-            EcptJump:=false;
-          except
-            on E:Exception do AddLog('Exception happened. Details : '+E.ClassName+':'+E.Message);
-          end;
-          if EcptJump then continue;
-          try
-            WriteDataToSQLite(tgr);
-            AddLog('Write database over.');
-          except
-            on E:Exception do AddLog('Exception happened. Details : '+E.ClassName+':'+E.Message);
-          end;
-        end;
-        AddLog('Calculation over. Now terminate.');
-      end;
-      -1:begin
-        AddLog('Start up command line error. Now terminate.')
-      end;
+  Paths=record
+    PName     : array[1..MaxJump] of string;    //跳转信道的名称
+    JumpNum   : integer;                        //总跳转数
+    TotalCost : integer;
   end;
-  CloseLog;
-  Terminate;
-end;
 
-constructor TConsoleServer.Create(TheOwner: TComponent);
-begin
-  inherited Create(TheOwner);
-  StopOnException:=True;
-end;
+var   //算是公用的变量吧，就写到这个单元里了，但是貌似要吃掉几十个M的内存……
+  Time,Cost : array[1..RouterNumber,1..RouterNumber,1..MaxTimeSection] of longint; //到达时间，跳转费用
+  PathName  : array[1..RouterNumber,1..RouterNumber,1..MaxTimeSection] of string;  //信道名称
+  f         : array[1..RouterNumber,1..MaxTimeSection] of longint;                 //计算数组
+  Path      : array[1..RouterNumber,1..MaxTimeSection] of integer;                 //跳转引导数组
+  Solve     : array[1..RouterNumber] of Paths;                                     //搜索出来的路径
+  Direct    : array[1..RouterNumber] of string;                                    //直接跳转信道
+  DCost     : array[1..RouterNumber] of integer;                                   //直接跳转代价
+  TotalTime : integer;
 
-destructor TConsoleServer.Destroy;
-begin
-  inherited Destroy;
-end;
+  Day1DB,Day2DB         : string;
+  Day1DBText,Day2DBText : string;
+  OutputDB              : string;
+  SysLogFile            : string;
 
-procedure TConsoleServer.WriteHelp;
-begin
-end;
+  SQLite3 : TSQLite3Dataset;
+  AOwner  : TComponent;
+  DataSrc : TDatasource;public;
+
+Procedure OpenLog;
+Procedure AddLog(msg:string);
+Procedure CloseLog;
+
+implementation
 
 var
-  Application: TConsoleServer;
+  logf:text;
 
-{$IFDEF WINDOWS}{$R SPS.rc}{$ENDIF}
-
+Procedure OpenLog;
 begin
-  Application:=TConsoleServer.Create(nil);
-  Application.Title:='ShortestPathSearch';
-  AOwner:=Application.Owner;
-  Application.Run;
-  Application.Free;
+  SysLogFile:=GetMonthString+'.log';
+  if FileExists(SysLogFile)=false then
+  begin
+    AssignFile(logf,SysLogFile);
+    Rewrite(logf);
+    CloseFile(logf);
+  end;
+  AssignFile(logf,SysLogFile);
+  Append(logf);
+  writeln(logf,'');
+  writeln(logf,'Ticket Search Project-Searching Kernel');
+  writeln(output,'Ticket Search Project-Searching Kernel');
+  writeln(logf,' By Enigma Huang, Version : '+ver);
+  writeln(output,' By Enigma Huang, Version : '+ver);
+end;
+
+Procedure AddLog(msg:string);
+begin
+  writeln(logf,AtDAT+msg);
+  writeln(output,AtDAT+msg);
+end;
+
+Procedure CloseLog;
+begin
+  CloseFile(logf);
+end;
+
 end.
-
